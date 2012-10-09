d12abf61ea4b22f8e757198f1b45ac3df6548c04
user <user@Centos.(none)>
2012-10-08 22:48:12 -0700

    Move new items and tests into index.coffee

--- index.js
+++ index.js
@@ -18,9 +18,6 @@ exports.attach = function() {
   this.helpers['container_fluid'] = function(t) {
     return div('.container-fluid', t);
   };
-  this.helpers['page_header'] = function(t) {
-    return div('.page-header', t);
-  };
   this.helpers['modal_header'] = function(t) {
     return div('.modal-header', t);
   };
@@ -30,16 +27,22 @@ exports.attach = function() {
   this.helpers['modal_footer'] = function(t) {
     return div('.modal-footer', t);
   };
+  this.helpers['modal'] = require('./modal');
+  this.helpers['modalLink'] = require('./modal_link');
+  this.helpers['modalHeaderCloseButton'] = require('./modal_header_close_button');
+  this.helpers['accordion'] = require('./accordion');
+  this.helpers['accordionEntry'] = require('./accordion_entry');
   this.helpers['row_fluid'] = function(t) {
     return div('.row-fluid', t);
   };
-  this.helpers['modal'] = require('./src/modal');
-  this.helpers['collaspeBtn'] = require('./collaspe_btn');
+  this.helpers['page_header'] = function(t) {
+    return div('.page-header', t);
+  };
+  this.helpers['collapseBtn'] = require('./collaspe_btn');
   this.helpers['brandLink'] = require('./brand_link');
   this.helpers['pageLink'] = require('./page_link');
-  this.helpers['modalLink'] = require('./src/modal_link');
-  this.helpers['accordionEntry'] = require('./src/accordion_entry');
-  this.helpers['accordion'] = require('./src/accordion');
-  this.helpers['modalHeaderCloseButton'] = require('./src/modal_header_close_button');
-  return this.helpers['dropdownMenu'] = require('./dropdown_menu');
+  this.helpers['dropdownMenu'] = require('./dropdown_menu');
+  this.helpers['jquery'] = require('./jquery');
+  this.helpers['bootstrapCSS'] = require('./bootstrapcss');
+  return this.helpers['bootstrapJS'] = require('./bootstrapjs');
 };
--- src/modal_header_close_button.coffee
+++ src/modal_header_close_button.coffee
@@ -1,4 +1,4 @@
 # output
 #<button type="button" class="close" data-dismiss="modal">&times;</button>
-module.exports = (name)  -> 
-  button "#{name}.close", 'type':'button', 'data-dismiss':'modal', '&times;'
+module.exports =  -> 
+  button ".close", 'type':'button', 'data-dismiss':'modal', '&times;'
